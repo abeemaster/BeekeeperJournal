04c37360a9aefa626f9a943ffcc1c0e053924dff
Олександр <a.beemaster@gmail.com>
2026-03-31 12:04:04 +0300

    "Версія 32.1.3. Змінено зовнішній вигляд діалогу вибору року + оновив build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.beemaster.beekeeperjournal"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.beemaster.beekeeperjournal"
--- app/src/main/java/com/beemaster/beekeeperjournal/dialogs/BeekeeperYearDialogFragment.kt
+++ app/src/main/java/com/beemaster/beekeeperjournal/dialogs/BeekeeperYearDialogFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
